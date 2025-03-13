3466e1741872545e0ef537d99aba3c822af6e117
Miguel Ángel Pérez. <92065472+lord-red-anubis@users.noreply.github.com>
2025-03-13 14:37:55 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("¡Hola desde Heroku!");
+// Servir archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta principal que muestra el archivo index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Iniciar el servidor
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+
